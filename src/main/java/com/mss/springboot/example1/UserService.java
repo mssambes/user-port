40c9ea7f916ad433d53107487dca871d972123c2
ambesgit <ambes@FullSie562c1828.fios-router.home>
2019-01-18 15:09:52 -0500

    starter code

---
+++ src/main/java/com/mss/springboot/example1/UserService.java
@@ -0,0 +1,16 @@
+package com.mss.springboot.example1;
+
+import java.util.List;
+
+public interface UserService {
+
+    User create(User user);
+
+    User delete(int id);
+
+    List<User> findAll();
+
+    User findById(int id);
+
+    User update(User user);
+}
